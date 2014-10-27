2d47de7b452c0a0d98d0a380ff9579be67ae8212
Bernhard Obereder <Bernhard.Obereder@stylight.com>
2014-10-27 14:36:48 +0100

    Cell now creates Segmented controlls from segments

--- Bureaucracy/Cells/SegmentedFormCell.swift
+++ Bureaucracy/Cells/SegmentedFormCell.swift
@@ -13,24 +13,28 @@ public class SegmentedFormCell: FormCell {
   @IBOutlet weak var segmentedControl: UISegmentedControl!
 
   @IBAction public func didChangeValue(sender: UISegmentedControl) {
-    if let field = formElement as? FormField<AnyObject, String, Int> {
-      field.value = sender.selectedSegmentIndex
+    if let field: SegmentedFormField = formElement as? SegmentedFormField<Int, Int, String> {
+      field.internalValue = sender.selectedSegmentIndex
     }
   }
 
   // MARK: - FormCell
 
   public override func update() {
-//    segmentedControl.removeAllSegments()
+    segmentedControl.removeAllSegments()
 
-//    if let field1 = formElement as? SegmentedFormField<Int, Int, String> {
-//      for title in field1.segments {
-//        segmentedControl.insertSegmentWithTitle(title, atIndex:segmentedControl.numberOfSegments, animated: false)
-//      }
-//      if let index = field.value{
-//        segmentedControl.selectedSegmentIndex = index
-//      }
-//    }
+    if let field = formElement as? SegmentedFormField<Int, Int, String> {
+      for title in field.segments {
+        segmentedControl.insertSegmentWithTitle(title, atIndex:segmentedControl.numberOfSegments, animated: false)
+      }
+      
+      if let index = field.internalValue{
+        segmentedControl.selectedSegmentIndex = index
+      }
+    }
  }
-
+  
+  public override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(false, animated: false)
+  }
 }
