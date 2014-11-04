a6fd03e46bb83ee73b8d86591d6e72aac7c87e79
Alexander Kolov <me@alexkolov.com>
2014-11-04 16:41:00 +0100

    Rename FormValidationError

---
+++ Bureaucracy/FormValidationError.swift
@@ -0,0 +1,21 @@
+//
+//  FormValidationError.swift
+//  Bureaucracy
+//
+//  Created by Alexander Kolov on 21.10.14.
+//  Copyright (c) 2014 Alexander Kolov. All rights reserved.
+//
+
+import UIKit
+
+public class FormValidationError: NSError {
+
+  init() {
+    super.init(domain: "com.bureaucracy", code: 1, userInfo: nil)
+  }
+
+  required public init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+}
