db9a4903e7c42456e12093b0af5c48f8608805ac
Alexander Kolov <me@alexkolov.com>
2014-11-04 19:21:27 +0100

    Form elements return tuples with name and value

--- Bureaucracy/Sections/FormSection.swift
+++ Bureaucracy/Sections/FormSection.swift
@@ -36,8 +36,16 @@ public class FormSection {
     return field
   }
 
-  public func values() -> [[String: Any?]] {
-    return elements.map { ($0 as FormElement).serialize() }
+  public func serialize() -> [String: Any?] {
+    return elements
+      .map { (var el) -> (String, Any?) in
+        el.serialize()
+      }
+      .reduce([:]) { (var dict, tuple) -> [String: Any?] in
+        var (key, value) = tuple
+        dict[key] = value
+        return dict
+      }
   }
 
 }
